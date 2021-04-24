8b22931d7ce67949df1ba88e9d4d081826eacac8
Paulo Vieira <paullocostta86@gmail.com>
2021-04-24 20:53:24 -0300

    feat: create list users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,171 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+} from '@chakra-ui/react'
+
+import Header from '../../components/Header'
+import Pagination from '../../components/Pagination'
+import Sidebar from '../../components/Sidebar'
+import { RiPencilLine } from '../../styles/icons'
+import CreateUser from './create'
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <CreateUser />
+          </Flex>
+
+          <Table colorScheme="whiteAlpha" variant="striped">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Paulo Vieira</Text>
+                    <Text
+                      fontWeight="sm"
+                      color="gray.300"
+                    >
+                      paullocostta86@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Paulo Vieira</Text>
+                    <Text
+                      fontWeight="sm"
+                      color="gray.300"
+                    >
+                      paullocostta86@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Paulo Vieira</Text>
+                    <Text
+                      fontWeight="sm"
+                      color="gray.300"
+                    >
+                      paullocostta86@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Paulo Vieira</Text>
+                    <Text
+                      fontWeight="sm"
+                      color="gray.300"
+                    >
+                      paullocostta86@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>25 de Abril, 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
